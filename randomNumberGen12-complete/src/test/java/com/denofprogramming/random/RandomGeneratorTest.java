706af43d1d105e09cd3f4deadbeac637347bdec0
dancer1325 <alfredotic0809@gmail.com>
2023-01-04 13:15:24 +0100

    fix(all): fix tests and switch to java version about maven-compile-plugin (Section2)

--- randomNumberGen12-complete/src/test/java/com/denofprogramming/random/RandomGeneratorTest.java
+++ randomNumberGen12-complete/src/test/java/com/denofprogramming/random/RandomGeneratorTest.java
@@ -30,7 +30,7 @@ public class RandomGeneratorTest
     @Test
     public void testName()
     {
-	Assert.assertSame("Name is not the same as expected: ", "Main Randon Number Generator", randomGenerator.name());
+	Assert.assertSame("Name is not the same as expected: ", "Main Random Number Generator", randomGenerator.name());
     }
 
     @Test
